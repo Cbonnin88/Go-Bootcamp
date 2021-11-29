9e931124f28909fc998e74276b7c73ee5c2e9002
Chris-Molotov <cbonnin@molotov.tv>
2021-11-29 10:00:49 +0100

    Finished Method lessons

--- Examples/Methods/main.go
+++ Examples/Methods/main.go
@@ -1,5 +1,8 @@
 package main
 
+import "Go-BootCamp/Examples/Methods/tickets"
+
 func main() {
+    tickets.PinoProject()
 
 }
--- Examples/Methods/tickets/pino.go
+++ Examples/Methods/tickets/pino.go
@@ -1,6 +1,8 @@
 package tickets
 
-import "fmt"
+import (
+    "fmt"
+)
 
 type Squad struct {
     Name      string
@@ -18,13 +20,39 @@ type Project struct {
 
 
 func (s Squad) Print() {
-    fmt.Printf("Name: %s\n", s.Name)
-    fmt.Printf("People: %s\n", s.People)
-    fmt.Printf("An active Sprint: %s\n", s.isSprint)
+    fmt.Printf("Squad Name: %s\n", s.Name)
+    fmt.Printf("Squad Amount: %d\n", s.People)
+    fmt.Printf("Project Name:%s\n", s.Project.Name)
+    fmt.Println("Technology used:", s.Project.Technology[0],s.Project.Technology[3])
+    fmt.Printf("Priority: %d\n", s.Project.Priority)
+    fmt.Printf("Points: %d\n", s.Project.Points)
+    fmt.Printf("An active Sprint: %t\n", s.isSprint)
+}
+
+func (s *Squad) UpdatePoints() int {
+     s.Project.Points += 2
+     return s.Project.Points
+}
+
+func (s *Squad) GetProjectPriority() int {
+    return s.Project.Priority
 }
 
 func PinoProject() {
-
+pino := Squad{
+       Name: "Pino",
+       People: 20,
+       isSprint: true,
+       Project: Project{
+                Name: "M6-Paywall",
+                Priority: 1,
+                Technology: []string{"Go","Php","JavaScript","Docker","React"},
+                Points: 13,
+       },
+    }
+    pino.Print()
+    fmt.Println("New priority point:",pino.UpdatePoints())
+    fmt.Println("Current Project Priority:",pino.GetProjectPriority())
 }
 
 
